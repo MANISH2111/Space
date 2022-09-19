fd63bf738e91568ab3ba19dfbe0a462f4f6e69c4
MANISH2111 <manish.sde173@gmail.com>
2022-09-19 17:06:55 +0530

    chore:filter red dot added ui fix

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,23 +1,29 @@
 import {
 	FlatList,
-	SafeAreaView,
 	StyleSheet,
 	Text,
 	TouchableOpacity,
 	View,
 } from 'react-native';
-import React, { useEffect, useCallback, useState, useRef } from 'react';
+import React, {
+	useEffect,
+	useCallback,
+	useState,
+	useRef,
+	FunctionComponent,
+	useMemo,
+} from 'react';
 import styled from 'styled-components/native';
 import { Entypo, FontAwesome } from '@expo/vector-icons';
 import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import BottomSheet, {
 	BottomSheetModal,
-	BottomSheetModalProvider,
 	BottomSheetBackdrop,
 } from '@gorhom/bottom-sheet';
 import { BottomSheetDefaultBackdropProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Spinner from 'react-native-loading-spinner-overlay';
+import { useDispatch } from 'react-redux';
 
 import { FlexCol, FlexRow } from '../../components/atom/Flex';
 import { useAppSelector } from '../../hooks';
@@ -27,7 +33,6 @@ import { SpaceCard } from './component/SpaceCard';
 import { Gutter } from '../../components/atom';
 import { SORT_DATA } from '../../data';
 import { AllFilter } from './component/filter';
-import { useDispatch } from 'react-redux';
 
 const CText = styled(Text)<{ isSelected?: Boolean }>`
 	font-size: 13px;
@@ -47,6 +52,14 @@ const NText = styled(Text)<{
 	text-align: ${(props) => (props.align ? props.align : `center`)};
 	margin-left: ${(props) => (props.mLeft ? `${props.mLeft}px` : 0)};
 `;
+const FBubble = styled(View)<{ isFilter?: Boolean }>`
+	width: 6px;
+	height: 6px;
+	border-radius: 3px;
+	margin-top: 18px;
+	margin-right: 10px;
+	background-color: ${(props) => (props.isFilter ? 'red' : 'transparent')};
+`;
 
 type Props = {
 	navigation: StackNavigationProp<MainStackParamList, 'SpaceX_Launches'>;
@@ -55,7 +68,7 @@ type Sort = {
 	sort?: String;
 };
 
-const Home: React.ComponentType<Props> = ({ navigation }) => {
+const Home: FunctionComponent<Props> = ({ navigation }) => {
 	const dispatch = useDispatch();
 
 	const data = useAppSelector((state) => state.launch.filteredLaunch);
@@ -71,8 +84,6 @@ const Home: React.ComponentType<Props> = ({ navigation }) => {
 
 	let x = Array.from(new Set(rocketArray));
 
-	
-
 	const SortObj: Sort = {
 		sort: '',
 	};
@@ -80,7 +91,7 @@ const Home: React.ComponentType<Props> = ({ navigation }) => {
 	const [value, setValue] = useState<String>('date');
 	const [spinner] = useState(loading);
 	const [sortObj, setSortObj] = useState(SortObj);
-
+	const [isFilter, setIsFliter] = useState<boolean>(false);
 	const handleSort = (val: String) => {
 		const obj = Object.assign({}, sortObj, {
 			sort: val,
@@ -110,6 +121,7 @@ const Home: React.ComponentType<Props> = ({ navigation }) => {
 		bottomFilterSheetModalRef.current?.snapToIndex(index);
 	}, []);
 
+	//HEADER
 	React.useLayoutEffect(() => {
 		navigation.setOptions({
 			headerRight: () => (
@@ -123,17 +135,13 @@ const Home: React.ComponentType<Props> = ({ navigation }) => {
 						/>
 					</TouchableOpacity>
 					<TouchableOpacity onPress={handlePresentFiterPress}>
-						<FontAwesome
-							style={styles.icon}
-							name="filter"
-							size={22}
-							color="grey"
-						/>
+						<FontAwesome name="filter" size={22} color="grey" />
 					</TouchableOpacity>
+					<FBubble isFilter={isFilter} />
 				</FlexRow>
 			),
 		});
-	}, [navigation]);
+	}, [navigation, isFilter]);
 
 	const renderBackdrop = useCallback(
 		(props: JSX.IntrinsicAttributes & BottomSheetDefaultBackdropProps) => (
@@ -151,8 +159,11 @@ const Home: React.ComponentType<Props> = ({ navigation }) => {
 		dispatch(getLaunches());
 	}, [dispatch]);
 
-	if (loading)
+	//LOADING SPINNER
+	if (loading) {
 		return <Spinner visible={spinner} textContent={'SpaceX_Loading...'} />;
+	}
+
 	return (
 		<>
 			<FlatList
@@ -164,77 +175,74 @@ const Home: React.ComponentType<Props> = ({ navigation }) => {
 				numColumns={2}
 			/>
 
-		
-				<BottomSheet
-					backdropComponent={renderBackdrop}
-					ref={bottomSortSheetModalRef}
-					index={-1}
-					snapPoints={[1, '25%']}
-					onChange={handleSortSheetChanges}
-				>
-					<>
-						<FlexRow justifyContent="space-between">
-							<NText mLeft="20" fSize={16} fWeight="bold">
-								Sort
-							</NText>
-							<Entypo
-								onPress={() =>
-									bottomSortSheetModalRef.current?.close()
-								}
-								name="cross"
-								size={24}
-								color="grey"
-								style={styles.icon}
-							/>
-						</FlexRow>
+			<BottomSheet
+				backdropComponent={renderBackdrop}
+				ref={bottomSortSheetModalRef}
+				index={-1}
+				snapPoints={[1, '25%']}
+				onChange={handleSortSheetChanges}
+			>
+				<>
+					<FlexRow justifyContent="space-between">
+						<NText mLeft="20" fSize={16} fWeight="bold">
+							Sort
+						</NText>
+						<Entypo
+							onPress={() =>
+								bottomSortSheetModalRef.current?.close()
+							}
+							name="cross"
+							size={24}
+							color="grey"
+							style={styles.icon}
+						/>
+					</FlexRow>
 
-						<Gutter spacing={1.3} />
+					<Gutter spacing={1.3} />
+					{/*SORTING VIEW */}
+					<FlexCol
+						justifyContent="space-around"
+						alignItems="flex-start"
+						style={styles.sort}
+					>
+						{SORT_DATA.map((item, index) => {
+							const name: String = item.value;
+							const isFocused = value === name;
 
-						<FlexCol
-							justifyContent="space-around"
-							alignItems="flex-start"
-							style={styles.sort}
-						>
-							{SORT_DATA.map((item, index) => {
-								const name: String = item.value;
-								const isFocused = value === name;
+							return (
+								<TouchableOpacity
+									key={index}
+									onPress={() => handleSort(name)}
+								>
+									<FlexRow>
+										<CText isSelected={isFocused}>
+											{item.name}
+										</CText>
+										<Gutter hSpacing={8} />
 
-								return (
-									<TouchableOpacity
-										key={index}
-										onPress={() => handleSort(name)}
-									>
-										<FlexRow>
-											<CText isSelected={isFocused}>
-												{item.name}
-											</CText>
-											<Gutter hSpacing={8} />
-
-											<Ionicons
-												name="checkmark-sharp"
-												size={24}
-												color={
-													isFocused
-														? '#148512'
-														: 'white'
-												}
-											/>
-										</FlexRow>
-										<Gutter spacing={1} />
-									</TouchableOpacity>
-								);
-							})}
-						</FlexCol>
-					</>
-				</BottomSheet>
-			
-
+										<Ionicons
+											name="checkmark-sharp"
+											size={24}
+											color={
+												isFocused ? '#148512' : 'white'
+											}
+										/>
+									</FlexRow>
+									<Gutter spacing={1} />
+								</TouchableOpacity>
+							);
+						})}
+					</FlexCol>
+				</>
+			</BottomSheet>
+			{/* FILTER VIEW */}
 			<AllFilter
 				filterArray={x}
 				handleFilterSheetChanges={handleFilterSheetChanges}
 				bottomFilterSheetModalRef={bottomFilterSheetModalRef}
 				renderBackdrop={renderBackdrop}
 				sorts={value}
+				setIsFilter={setIsFliter}
 			/>
 		</>
 	);
@@ -257,6 +265,11 @@ const styles = StyleSheet.create({
 	},
 	sort: {
 		marginHorizontal: '20%',
+	},
+	nodata: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		marginTop: '50%',
 	},
 });
 
--- src/screens/Home/component/filter/index.tsx
+++ src/screens/Home/component/filter/index.tsx
@@ -1,12 +1,16 @@
-import React, { FunctionComponent, useCallback, useState } from 'react';
+import React, {
+	Dispatch,
+	FunctionComponent,
+	SetStateAction,
+	useCallback,
+	useMemo,
+	useState,
+} from 'react';
 import styled from 'styled-components/native';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { FilterButton } from './Filter';
 import { FlexCol, FlexRow, Gutter } from '../../../../components/atom';
-import BottomSheet, {
-	BottomSheetBackdropProps,
-
-} from '@gorhom/bottom-sheet';
+import BottomSheet, { BottomSheetBackdropProps } from '@gorhom/bottom-sheet';
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { useDispatch } from 'react-redux';
@@ -75,6 +79,8 @@ type Props = {
 	renderBackdrop: FunctionComponent<BottomSheetBackdropProps>;
 	handleFilterSheetChanges: (index: number) => void;
 	sorts?: String;
+	isFilter?: Boolean;
+	setIsFilter?: Dispatch<SetStateAction<boolean>>;
 };
 
 const AllFilter: React.ComponentType<Props> = ({
@@ -83,6 +89,7 @@ const AllFilter: React.ComponentType<Props> = ({
 	renderBackdrop,
 	handleFilterSheetChanges,
 	sorts,
+	setIsFilter,
 }) => {
 	const FObj: Filter = {
 		rockets: [],
@@ -104,7 +111,6 @@ const AllFilter: React.ComponentType<Props> = ({
 	const [isDateVsisble, setDateVisible] = useState(false);
 	const [date, setDate] = useState({ StartDate: null, EndDate: null });
 
-
 	const showDatePicker = (index: number) => {
 		index === 0 ? setDatePickerVisibility(true) : setDateVisible(true);
 	};
@@ -113,22 +119,7 @@ const AllFilter: React.ComponentType<Props> = ({
 		index === 0 ? setDatePickerVisibility(false) : setDateVisible(false);
 	};
 
-	const handleDateConfirm = useCallback(
-		(index: number, value: Date) => {
-			try {
-				setDate((prevState) => ({
-					...prevState,
-					[index === 0 ? 'StartDate' : 'EndDate']: value
-						? moment(value).format()
-						: null,
-				}));
-			} catch (error) {
-			} finally {
-				hideDatePicker(index);
-			}
-		},
-		[date],
-	);
+	//FILTER METHODS
 	const handleRocketFilter = useCallback(
 		(item: any) => {
 			const index = rocketsFilter.findIndex((x: any) => x === item);
@@ -172,7 +163,7 @@ const AllFilter: React.ComponentType<Props> = ({
 		},
 		[success],
 	);
-
+	//RESET METHOD
 	const handleRest = useCallback(() => {
 		try {
 			setRocketsFilter([]);
@@ -182,9 +173,14 @@ const AllFilter: React.ComponentType<Props> = ({
 
 			dispatch(resetLaunches());
 			bottomFilterSheetModalRef.current?.close();
-		} catch (error) {}
+		} catch (error) {
+		} finally {
+			//@ts-ignore
+			setIsFilter(false);
+		}
 	}, [dispatch, FObj]);
 
+	//ALL FILTER DISPATCH
 	const handleFilter = useCallback(
 		(
 			newRocketFilters: any,
@@ -200,6 +196,9 @@ const AllFilter: React.ComponentType<Props> = ({
 					time: newTime,
 				});
 				setFilterObj(obj);
+				//@ts-ignore
+				setIsFilter(JSON.stringify(filterObj) !== JSON.stringify(obj));
+
 				dispatch(filterLaunches(obj));
 			} catch (error) {
 			} finally {
@@ -208,6 +207,37 @@ const AllFilter: React.ComponentType<Props> = ({
 			}
 		},
 		[dispatch, FObj, sorts],
+	);
+	//DATE FILTER
+	const handleDateConfirm = useCallback(
+		(index: number, value: Date) => {
+			try {
+				setDate((prevState) => ({
+					...prevState,
+					[index === 0 ? 'StartDate' : 'EndDate']: value
+						? moment(value).format()
+						: null,
+				}));
+			} catch (error) {
+			} finally {
+				hideDatePicker(index);
+			}
+		},
+		[date],
+	);
+	const x = Object.values(date);
+	const y = Object.keys(date);
+
+	const dateView = useCallback(
+		(index: number) => (
+			<NText color="grey">
+				{x[index] === null
+					? y[index]
+					: moment(x[index]).format('MMM D, YYYY')}
+			</NText>
+		),
+
+		[date],
 	);
 
 	return (
@@ -218,181 +248,165 @@ const AllFilter: React.ComponentType<Props> = ({
 			snapPoints={[1, '75%']}
 			onChange={handleFilterSheetChanges}
 		>
-		
-				<FlexRow justifyContent="space-between">
-					<NText
-						mLeft="15"
-						align="left"
-						color="black"
-						fSize={16}
-						fWeight="bold"
-					>
-						Filters
-					</NText>
-					<Entypo
-						onPress={() =>
-							bottomFilterSheetModalRef.current?.close()
-						}
-						name="cross"
-						size={24}
-						color="grey"
-						style={styles.icon}
-					/>
-				</FlexRow>
-				<Gutter />
+			<FlexRow justifyContent="space-between">
 				<NText
-					mLeft="12"
-					fSize={16}
-					color="#666666"
-					fWeight="500"
+					mLeft="15"
 					align="left"
+					color="black"
+					fSize={16}
+					fWeight="bold"
 				>
-					Rocket Name
+					Filters
 				</NText>
+				<Entypo
+					onPress={() => bottomFilterSheetModalRef.current?.close()}
+					name="cross"
+					size={24}
+					color="grey"
+					style={styles.icon}
+				/>
+			</FlexRow>
+			<Gutter />
+			<NText
+				mLeft="12"
+				fSize={16}
+				color="#666666"
+				fWeight="500"
+				align="left"
+			>
+				Rocket Name
+			</NText>
+			{/* ROCKET FILTER */}
+			<RocketButton>
+				{filterArray.map((item: String, index: number) => {
+					return (
+						<>
+							<FilterButton
+								key={index}
+								name={item}
+								isSelected={
+									rocketsFilter.findIndex(
+										(x: String) => x === item,
+									) > -1
+								}
+								onPress={() => handleRocketFilter(item)}
+							/>
+						</>
+					);
+				})}
 
-				<RocketButton>
-					{filterArray.map((item: String, index: number) => {
-						return (
-							<>
-								<FilterButton
-									key={index}
-									name={item}
-									isSelected={
-										rocketsFilter.findIndex(
-											(x: String) => x === item,
-										) > -1
-									}
-									onPress={() => handleRocketFilter(item)}
-								/>
-							</>
-						);
-					})}
-
-					<Gutter spacing={4} />
-					<FlexCol>
-						<NText
-							fSize={16}
-							color="#666666"
-							fWeight="500"
-							align="left"
-						>
-							Upcoming
-						</NText>
-						<FlexRow alignItems="flex-start">
-							{ROCKET_UPCOMING.map((xx, i) => {
-								return (
-									<FilterButton
-										key={i}
-										name={xx}
-										isSelected={
-											upcomingFilter.findIndex(
-												(x: String) => x === xx,
-											) > -1
-										}
-										onPress={() => handleUpcomingFilter(xx)}
-									/>
-								);
-							})}
-						</FlexRow>
-					</FlexCol>
-					<Gutter spacing={5} />
+				<Gutter spacing={4} />
+				{/* UPCOMING FILTER */}
+				<FlexCol>
 					<NText
 						fSize={16}
 						color="#666666"
 						fWeight="500"
 						align="left"
 					>
-						Launch Status
+						Upcoming
 					</NText>
-					<FlexRow>
-						{LAUNCH_SUCCESS.map((y, i) => {
+					<FlexRow alignItems="flex-start">
+						{ROCKET_UPCOMING.map((xx, i) => {
 							return (
 								<FilterButton
-									key={i}
-									name={y}
+									key={xx}
+									name={xx}
 									isSelected={
-										success.findIndex(
-											(x: String) => x === y,
+										upcomingFilter.findIndex(
+											(x: String) => x === xx,
 										) > -1
 									}
-									onPress={() => handleRocketSuccess(y)}
+									onPress={() => handleUpcomingFilter(xx)}
 								/>
 							);
 						})}
 					</FlexRow>
-				</RocketButton>
-				<Gutter spacing={1} />
-				<NText
-					fSize={16}
-					color="#666666"
-					fWeight="500"
-					align="left"
-					mLeft="15"
-				>
-					Date Range
+				</FlexCol>
+				<Gutter spacing={5} />
+				{/* LAUNCH SATATUS FILTER */}
+				<NText fSize={16} color="#666666" fWeight="500" align="left">
+					Launch Status
 				</NText>
-				<Gutter />
-				<FlexRow justifyContent="center">
-					{Object.keys(date).map((item, index) => {
+				<FlexRow>
+					{LAUNCH_SUCCESS.map((y, i) => {
 						return (
-							<>
-								<DButton
-									key={index}
-									onPress={() => showDatePicker(index)}
-								>
-									<NText color="grey">
-										{Object.values(date)[index] === null
-											? index === 0
-												? 'Start Date'
-												: 'End Date'
-											: moment(
-													Object.values(date)[index],
-											  ).format('MMM D, YYYY')}
-									</NText>
-								</DButton>
-
-								<DateTimePickerModal
-									isVisible={
-										index === 0
-											? isDatePickerVisible
-											: isDateVsisble
-									}
-									mode="date"
-									onConfirm={(val) =>
-										handleDateConfirm(index, val)
-									}
-									onCancel={() => hideDatePicker(index)}
-									
-								/>
-							</>
+							<FilterButton
+								key={y}
+								name={y}
+								isSelected={
+									success.findIndex((x: String) => x === y) >
+									-1
+								}
+								onPress={() => handleRocketSuccess(y)}
+							/>
 						);
 					})}
 				</FlexRow>
-				<Gutter spacing={2} />
-				<FlexRow justifyContent="space-evenly" alignItems="center">
-					<NButton onPress={() => handleRest()}>
-						<NText fSize={20} fWeight="400" color="black">
-							Reset
-						</NText>
-					</NButton>
-					<NButton
-						bgColor="black"
-						onPress={() =>
-							handleFilter(
-								rocketsFilter,
-								upcomingFilter,
-								success,
-								date,
-							)
-						}
-					>
-						<NText fSize={20} fWeight="400" color="white">
-							Done
-						</NText>
-					</NButton>
-				</FlexRow>
-				<Gutter />
-		
+			</RocketButton>
+			<Gutter spacing={1} />
+			{/* DATE RANGE FILTER */}
+			<NText
+				fSize={16}
+				color="#666666"
+				fWeight="500"
+				align="left"
+				mLeft="15"
+			>
+				Date Range
+			</NText>
+			<Gutter />
+			<FlexRow justifyContent="center">
+				{[date.StartDate, date.EndDate].map((item, index) => {
+					return (
+						<>
+							<DButton
+								key={item}
+								onPress={() => showDatePicker(index)}
+							>
+								{dateView(index)}
+							</DButton>
+
+							<DateTimePickerModal
+								isVisible={
+									index === 0
+										? isDatePickerVisible
+										: isDateVsisble
+								}
+								mode="date"
+								onConfirm={(val) =>
+									handleDateConfirm(index, val)
+								}
+								onCancel={() => hideDatePicker(index)}
+							/>
+						</>
+					);
+				})}
+			</FlexRow>
+			<Gutter spacing={2} />
+			<FlexRow justifyContent="space-evenly" alignItems="center">
+				<NButton onPress={() => handleRest()}>
+					<NText fSize={20} fWeight="400" color="black">
+						Reset
+					</NText>
+				</NButton>
+				<NButton
+					bgColor="black"
+					onPress={() =>
+						handleFilter(
+							rocketsFilter,
+							upcomingFilter,
+							success,
+							date,
+						)
+					}
+				>
+					<NText fSize={20} fWeight="400" color="white">
+						Done
+					</NText>
+				</NButton>
+			</FlexRow>
+			<Gutter />
 		</BottomSheet>
 	);
 };
